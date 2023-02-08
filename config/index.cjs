ae3cf9739b696d7f8f7abe1a0e2d70e7983e3efc
yaoxaioxin0729 <124783352+yaoxaioxin0729@users.noreply.github.com>
2023-02-08 19:40:19 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -54,7 +54,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: ' 07d3a106a4fc1aea54589fa8d7c26dec',
+      id: ' oVHVO5s8a5pcDrqzy-qFc1PNk2rk',
     }
   ],
 
